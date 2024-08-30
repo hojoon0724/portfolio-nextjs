19f9454467da340154a4fec9af3e7ef80832d239
hojoon0724 <hojoon0724@gmail.com>
2024-08-30 16:59:25 -0700

    changed the descriptions

--- src/data/designProjectsData.js
+++ src/data/designProjectsData.js
@@ -4,7 +4,7 @@ export const designProjectsData = [
   {
     title: 'Rocket City New Music',
     logo: '/rcnm/rcnm-lockupLong.svg',
-    text: 'DescriptionLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    text: 'In 2021, I co-founded this contemporary classical music concert series in Huntsville, home of rocket engines that took us to the moon. I created the visual identity and I make the events more immersive by using lighting and visual elements to enhance the audience experience.',
     stack: [
       iconsData.javascript,
       iconsData.html5,
