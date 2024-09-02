dbc81697b0e4a360b90c2c647fe8b43fc13b505c
hojoon0724 <hojoon0724@gmail.com>
2024-09-02 13:29:41 -0700

    navbar css migrated

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,39 +1,40 @@
+import style from './navbar.module.css';
 import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <div className="nav-bar-container flex center">
-      <div className="nav-bar flex row">
+    <div className={`${style['nav-bar-container']} flex center`}>
+      <div className={`${style['nav-bar']} flex row`}>
         {/* <div className="nav-bar-left">
           <Link href="/">
             <div className="logo">Hojoon Kim</div>
             <div className="subtitle">Design-centered Developer</div>
           </Link>
         </div> */}
-        <div className="nav-bar-center">
-          <div className="nav-btn-container flex center">
-            <div className="nav-btn">
+        <div className={`${style['nav-bar-center']}`}>
+          <div className={`${style['nav-btn-container']} flex center`}>
+            <div className={`${style['nav-btn']}`}>
               <Link href="#tech">tech</Link>
             </div>
-            <div className="nav-btn-bg "></div>
+            <div className={`${style['nav-btn-bg']}`}></div>
           </div>
-          <div className="nav-btn-container flex center">
-            <div className="nav-btn">
+          <div className={`${style['nav-btn-container']} flex center`}>
+            <div className={`${style['nav-btn']}`}>
               <Link href="#design">design</Link>
             </div>
-            <div className="nav-btn-bg "></div>
+            <div className={`${style['nav-btn-bg']}`}></div>
           </div>
-          <div className="nav-btn-container flex center">
-            <div className="nav-btn">
+          <div className={`${style['nav-btn-container']} flex center`}>
+            <div className={`${style['nav-btn']}`}>
               <Link href="#about">about</Link>
             </div>
-            <div className="nav-btn-bg "></div>
+            <div className={`${style['nav-btn-bg']}`}></div>
           </div>
-          <div className="nav-btn-container flex center">
-            <div className="nav-btn">
+          <div className={`${style['nav-btn-container']} flex center`}>
+            <div className={`${style['nav-btn']}`}>
               <Link href="#contact">contact</Link>
             </div>
-            <div className="nav-btn-bg "></div>
+            <div className={`${style['nav-btn-bg']}`}></div>
           </div>
         </div>
       </div>
