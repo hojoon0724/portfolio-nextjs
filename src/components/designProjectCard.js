8e7a56c21b2b4edeed42756d96865465bb7a9b00
hojoon0724 <hojoon0724@gmail.com>
2024-09-02 23:35:00 -0400

    moved some project css only for design projects

--- src/components/designProjectCard.js
+++ src/components/designProjectCard.js
@@ -1,3 +1,4 @@
+import style from '@/sections/design.module.css';
 import StackIcon from '@/components/stackIcon';
 import LinkButton from '@/components/linkButton';
 import MainLinkButton from './mainLinkButton';
@@ -5,36 +6,38 @@ import ProjectLogo from './projectLogo';
 
 export default function DesignProjectCard({ project }) {
   return (
-    <div className="project-container flex column">
-      <div className="project-screenshot"></div>
-      <div className="project-text-container">
-        <div className="project-title">{project.logo ? <ProjectLogo project={project} /> : project.title}</div>
-        <div className="project-tags flex">
+    <div className={`${style['design-project-container']} flex column`}>
+      <div className={`${style['project-screenshot']}`}></div>
+      <div className={`${style['project-text-container']}`}>
+        <div className={`${style['design-project-title']}`}>
+          {project.logo ? <ProjectLogo project={project} /> : project.title}
+        </div>
+        <div className={`${style['design-project-tags']} flex`}>
           {project.tags.map(tag => (
-            <div className="tag-item" key={tag}>
+            <div className={`${style['design-tag-item']}`} key={tag}>
               {tag}
             </div>
           ))}
         </div>
-        <div className="project-about">
+        <div className={`${style['design-project-about']}`}>
           {project.text.split('\n').map((para, index) => (
             <p key={index}>{para}</p>
           ))}
         </div>
-        <div className="main-link-container">
+        <div className={`${style['design-main-link-container']}`}>
           <MainLinkButton targetLink={project.mainLink} />
         </div>
       </div>
-      <div className="buttons-container">
-        <div className="made-with" style={{ width: '100%' }}>
-          <div className="made-with-text">Tools used:</div>
-          <div className="stack-icons-container flex">
+      <div className={`${style['design-buttons-container']}`}>
+        <div className={`${style['design-made-with']}`} style={{ width: '100%' }}>
+          <div className={`${style['design-made-with-text']}`}>Tools used:</div>
+          <div className={`${style['design-stack-icons-container flex']}`}>
             {project.stack.map(tech => (
               <StackIcon key={tech.name} itemName={tech} />
             ))}
           </div>
         </div>
-        <div className="repo-links-container">
+        <div className={`${style['design-repo-links-container']}`}>
           {project.repoLinks.map(link => (
             <LinkButton key={link.url} bgColor={'white'} padding={'0px'} targetLink={link} />
           ))}
