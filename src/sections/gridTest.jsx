e67724ebcce87bc37d3e47bda5c0aa68fa7906e2
Hojoon Kim <hojoon0724@gmail.com>
2024-12-25 19:00:38 -0800

    added more cards and positions

--- src/sections/gridTest.jsx
+++ src/sections/gridTest.jsx
@@ -3,13 +3,20 @@ import styled from 'styled-components';
 import DesignProjectCardSmall from '@/components/designProjectCardSmall';
 import { motion } from 'framer-motion';
 import { designProjectsData } from '@/data/designProjectsData';
+import styles from '@/sections/gridTest.module.css';
 
 const layoutLg = [
-  { i: 'rcnm', x: 0, y: 0, w: 1, h: 1 },
-  { i: 'partner-energy', x: 1, y: 0, w: 1, h: 1 },
-  { i: 'ensrq', x: 2, y: 0, w: 1, h: 1 },
-  { i: 'lamongrel', x: 0, y: 1, w: 1, h: 1 },
-  { i: 'shit-on-the-floor', x: 1, y: 1, w: 2, h: 1 },
+  { i: 'intro', x: 0, y: 0, w: 3, h: 1.5 },
+
+  { i: 'hourglass', x: 0, y: 1, w: 2, h: 1 },
+  { i: 'pacific-serenades', x: 2, y: 1, w: 1, h: 1 },
+  { i: 'blackjack', x: 1, y: 2, w: 2, h: 1 },
+  { i: 'bkmrkd', x: 0, y: 2, w: 1, h: 1 },
+
+  { i: 'rcnm', x: 0, y: 3, w: 2, h: 1 },
+  { i: 'partner-energy', x: 2, y: 3, w: 1, h: 1 },
+  { i: 'ensrq', x: 0, y: 4, w: 1, h: 1 },
+  { i: 'lamongrel', x: 1, y: 4, w: 2, h: 1 },
 ];
 
 // Styled component for GridLayout
@@ -18,14 +25,17 @@ const StyledGridLayout = styled(GridLayout)`
   margin: 0 auto;
   width: 100%;
   max-width: 1200px;
-  border: 5px solid red;
+  border: 0px solid red;
   display: grid;
 `;
 
 const GridItemWrapper = styled.div`
-  background: #888888;
+  background: #222222;
   border-radius: 24px;
   padding: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Root = styled.div`
@@ -34,7 +44,20 @@ const Root = styled.div`
   display: flex;
   justify-content: center;
   height: 100%;
+  margin: 0 auto;
 `;
+
+const shortBio = (
+  <div>
+    <p className="text-mono-small">
+      I’m a <span className="text-yellow">design-centered</span>
+      <span className="text-pink"> software engineer </span>
+      with a background in
+      <span className="text-light-blue"> design, research, and consulting </span>
+      and I build systemic solutions for people.
+    </p>
+  </div>
+);
 
 export function Grid() {
   const data = designProjectsData;
@@ -50,6 +73,29 @@ export function Grid() {
           isDraggable={false}
           isResizable={false}
         >
+          <GridItemWrapper key="hourglass">
+            <div className="design-project-container">
+              <h2>Hourglass</h2>
+            </div>
+          </GridItemWrapper>
+          <GridItemWrapper key="pacific-serenades">
+            <div className="design-project-container">
+              <h2>PacSer Website</h2>
+            </div>
+          </GridItemWrapper>
+          <GridItemWrapper key="blackjack">
+            <div className="design-project-container">
+              <h2>Blackjack Card Counter</h2>
+            </div>
+          </GridItemWrapper>
+          <GridItemWrapper key="bkmrkd">
+            <div className="design-project-container">
+              <h2>bkmrkd</h2>
+            </div>
+          </GridItemWrapper>
+          <GridItemWrapper>
+            <h2>Design Projects</h2>
+          </GridItemWrapper>
           <GridItemWrapper key="rcnm">
             <DesignProjectCardSmall project={data[0]} />
           </GridItemWrapper>
@@ -62,8 +108,15 @@ export function Grid() {
           <GridItemWrapper key="lamongrel">
             <DesignProjectCardSmall project={data[3]} />
           </GridItemWrapper>
-          <GridItemWrapper key="shit-on-the-floor">
-            <DesignProjectCardSmall project={data[3]} />
+          <GridItemWrapper key="intro">
+            <div className="design-project-container">
+              <div className={styles.helloContainer}>
+                <div className={`text-mono-small`}>{`Hi, my name is`}</div>
+                <div className={`${styles.hojoon}`}>{`Hojoon Kim`}</div>
+                <div className={`${styles.intro} text-mono-small`}>{`I design and build apps and stuff`}</div>
+                <div className={`${styles.shortBio} text-mono-small`}>{shortBio}</div>
+              </div>
+            </div>
           </GridItemWrapper>
         </StyledGridLayout>
       </Root>
