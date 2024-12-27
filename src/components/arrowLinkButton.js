d912a912557c5b8f3d19e06a2a101e02266f9935
Hojoon Kim <hojoon0724@gmail.com>
2024-12-26 19:32:41 -0800

    button arrow animation done

--- src/components/arrowLinkButton.js
+++ src/components/arrowLinkButton.js
@@ -3,12 +3,10 @@ import ButtonIcon from './buttonIcon';
 export default function ArrowLinkButton({ targetLink }) {
   return (
     <a href={targetLink.url} target={targetLink.openOnNewWindow ? '_blank' : '_self'} className={'pri-black-btn'}>
-      <div className="arrow-icon-container">
         <div className="arrowIconBg arrow-button-icon">
           <img src={`/assets/icons/arrowBlack.svg`} alt={targetLink.description} />
         </div>
-      </div>
-      <div className="link-text">{targetLink.button}</div>
+      {targetLink.text === '' ? '' : <div className="link-text">{targetLink.text}</div>}
     </a>
   );
 }
--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -1,10 +1,13 @@
+import ArrowLinkButton from '@/components/arrowLinkButton';
 import styles from './index.module.css';
 import { Grid } from '@/sections/gridTest';
 
 export default function CardWithBorder({}) {
+  const testTargetLink = { targetLink: 'google.com', openOnNewWindow: false, description: 'text', text: 'Google' };
+
   return (
-    <div className="top-container">
-      
+    <div className="top-container flex justify-center">
+      <ArrowLinkButton targetLink={testTargetLink} />
     </div>
     // <section className={styles.sectionContainer}>
     //   <div className={styles.cardContainer}>
