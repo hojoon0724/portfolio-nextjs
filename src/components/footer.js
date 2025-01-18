6532a3d764f5dc68ba3fd76cd041195c7154c2d8
Hojoon Kim <hojoon0724@gmail.com>
2025-01-17 19:08:57 -0800

    migrated footer css to styled-components

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,31 @@
-import style from './footer.module.css';
+import styled from 'styled-components';
+
+const FooterContainer = styled.div`
+  width: 100%;
+  font-weight: 400;
+  letter-spacing: 0.1ch;
+  bottom: 0px;
+  box-shadow: 0px -4px 8px 0px hsla(0, 0%, 0%, 0.25);
+  color: rgb(255, 255, 255);
+  background-color: hsla(0, 0%, 60%, 0.25);
+  isolation: isolate;
+  position: absolute; 
+  backdrop-filter: blur(3rem) saturate(165%);
+  z-index: 100;
+`;
+
+const FooterInner = styled.div`
+  text-shadow: 0px 0px 2px var(--black);
+  width: 100%;
+  padding: 8px;
+  font-size: 8pt;
+  position: sticky;
+`;
 
 export default function Footer() {
   return (
-    <div className="footer-container flex items-center justify-center">
-      <div className="footer flex items-center justify-center">
+    <FooterContainer className="footer-container flex items-center justify-center">
+      <FooterInner className="footer flex items-center justify-center">
         <span style={{ color: 'var(--blue)' }}>&#91;&nbsp;</span>
         <span style={{ color: 'var(--pink)' }}>designed&nbsp;</span>
         <span style={{ color: 'var(--white)' }}>and&nbsp;</span>
@@ -13,7 +35,7 @@ export default function Footer() {
         <span style={{ color: 'var(--yellow)' }}>&#123;&nbsp;</span>
         <span style={{ color: 'var(--light-blue)' }}>Hojoon Kim&nbsp;</span>
         <span style={{ color: 'var(--yellow)' }}>&#125;</span>
-      </div>
-    </div>
+      </FooterInner>
+    </FooterContainer>
   );
 }
